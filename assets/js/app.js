fb57ed14dc71060895302885d34a24632f4450ef
Arish Ali <109577410+arish0201@users.noreply.github.com>
2023-03-09 00:34:11 +0500

    all changes done that was given by trilok

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,9 +22,13 @@
 // 19. image gallery js
 // 20. contact form js
 
+// Hamburger
+$('.hamburger').click(function() {
+  $( this ).toggleClass( "active" );
+});
 // dark mode/ light mode
-$(document).ready(function() {
-  $("#switch_theme_mode").click(function() {
+$(document).ready(function () {
+  $("#switch_theme_mode").click(function () {
     var stylesheet = $("#stylesheet");
     var altStylesheet = $("#alt-stylesheet");
     if (stylesheet.prop("disabled")) {
@@ -40,12 +44,12 @@ $(document).ready(function() {
 // Navlinks active class
 
 $(document).ready(function () {
-  $('a.nav-link').click(function(e) {
+  $('a.nav-link').click(function (e) {
 
-      $('.nav-item.active').removeClass('active');
+    $('.nav-item.active').removeClass('active');
 
-      var $parent = $(this).parent();
-      $parent.addClass('active');
+    var $parent = $(this).parent();
+    $parent.addClass('active');
   });
 });
 
@@ -106,16 +110,7 @@ jQuery(function ($) {
     pageContainer: $('.container'),
     breakpoint: 767.98,
     hideTimeOut: 0
-  }); // 3. fixed navbar
-
-  // $(window).on('scroll', function () {
-  //   // checks if window is scrolled more than 500px, adds/removes solid class
-  //   if ($(this).scrollTop() > 100) {
-  //     $('.main-header-menu-wrap').addClass('affix');
-  //   } else {
-  //     $('.main-header-menu-wrap').removeClass('affix');
-  //   }
-  // }); // 4. scroll bottom to top
+  }); 
 
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > $(window).height()) {
@@ -138,171 +133,9 @@ jQuery(function ($) {
         }
       });
     }
-  }); // 5. custom vps hosting plan js
-  //**************************** Need to change ********************
-  // Change according to your plans
-  //****************************************************************
-  // vpsPlan      = VPS plan or package number
-  // vpsCore      = VPS Core value from your plan
-  // vpsMemory    = VPS Memory from your plan
-  // vpsStorage   = VPS Storage value from your plan
-  // vpsBandwidth = VPS bandwidth value from your plan
-  // vpsPrice     = VPS price from your plan
-  // vpsWHmcsUrl  = VPS plan url. Check from your WHMCS panel
-  //****************************************************************
+  }); 
 
-  var vpsPriceInfo = [{
-    "vpsPlan": 1,
-    "vpsCore": 1,
-    "vpsMemory": 2,
-    "vpsStorage": 10,
-    "vpsBandwidth": 1000,
-    "vpsPrice": 20,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=12"
-  }, {
-    "vpsPlan": 2,
-    "vpsCore": 2,
-    "vpsMemory": 4,
-    "vpsStorage": 20,
-    "vpsBandwidth": 2000,
-    "vpsPrice": 40,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=13"
-  }, {
-    "vpsPlan": 3,
-    "vpsCore": 3,
-    "vpsMemory": 6,
-    "vpsStorage": 30,
-    "vpsBandwidth": 3000,
-    "vpsPrice": 60,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=14"
-  }, {
-    "vpsPlan": 4,
-    "vpsCore": 4,
-    "vpsMemory": 8,
-    "vpsStorage": 40,
-    "vpsBandwidth": 4000,
-    "vpsPrice": 80,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=13"
-  }, {
-    "vpsPlan": 5,
-    "vpsCore": 5,
-    "vpsMemory": 10,
-    "vpsStorage": 50,
-    "vpsBandwidth": 5000,
-    "vpsPrice": 100,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=12"
-  }, {
-    "vpsPlan": 6,
-    "vpsCore": 6,
-    "vpsMemory": 12,
-    "vpsStorage": 60,
-    "vpsBandwidth": 6000,
-    "vpsPrice": 120,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=13"
-  }, {
-    "vpsPlan": 7,
-    "vpsCore": 7,
-    "vpsMemory": 14,
-    "vpsStorage": 70,
-    "vpsBandwidth": 7000,
-    "vpsPrice": 140,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=14"
-  }, {
-    "vpsPlan": 8,
-    "vpsCore": 8,
-    "vpsMemory": 16,
-    "vpsStorage": 80,
-    "vpsBandwidth": 8000,
-    "vpsPrice": 160,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=12"
-  }, {
-    "vpsPlan": 9,
-    "vpsCore": 9,
-    "vpsMemory": 18,
-    "vpsStorage": 90,
-    "vpsBandwidth": 9000,
-    "vpsPrice": 180,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=13"
-  }, {
-    "vpsPlan": 10,
-    "vpsCore": 10,
-    "vpsMemory": 20,
-    "vpsStorage": 100,
-    "vpsBandwidth": 10000,
-    "vpsPrice": 200,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=14"
-  }, {
-    "vpsPlan": 11,
-    "vpsCore": 11,
-    "vpsMemory": 22,
-    "vpsStorage": 110,
-    "vpsBandwidth": 11000,
-    "vpsPrice": 220,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=12"
-  }, {
-    "vpsPlan": 12,
-    "vpsCore": 12,
-    "vpsMemory": 24,
-    "vpsStorage": 120,
-    "vpsBandwidth": 12000,
-    "vpsPrice": 240,
-    "vpsWHmcsUrl": "https://whmcs.themetags.com/cart.php?a=add&pid=13"
-  }];
-  var cPlan = $('#c-plan');
-
-  if (cPlan.length) {
-    cPlan.slider({
-      tooltip: 'always'
-    });
-    cPlan.on("slide", function (e) {
-      $.each(vpsPriceInfo, function (index, vpsObj) {
-        if (vpsObj.vpsPlan == e.value) {
-          setVpsValue(vpsObj);
-        }
-      });
-    });
-    initSlider();
-  }
-
-  function initSlider() {
-    cPlan.value = cPlan.data("slider-value");
-    var defaultVpsCore = parseInt(cPlan.value);
-    $.each(vpsPriceInfo, function (index, vpsObj) {
-      // defaultVpsCore is default valeu to show. For my demo, I have set 6 from HTML.
-      // vps-hosting.html: <input id="c-plan" type="text" data-slider-min="1" data-slider-max="12" data-slider-step="1" data-slider-value="6" data-currency="$" data-unit="GB">. 
-      // You need to change accourting to your need.
-      if (vpsObj.vpsPlan == defaultVpsCore) {
-        $('.slider .tooltip', '#custom-plan').append('<div class="tooltip-up"></div>');
-        $('.slider .tooltip-inner', '#custom-plan').attr("data-unit", cPlan.data("unit"));
-        $('.slider .tooltip-up', '#custom-plan').attr("data-currency", cPlan.data("currency"));
-        setVpsValue(vpsObj);
-      }
-    });
-  }
-
-  function setVpsValue(vpsObj) {
-    $('.slider .tooltip-up', '#custom-plan').text(vpsObj.vpsPrice);
-    $('.vpsPrice', '#custom-plan').text(cPlan.data("currency") + vpsObj.vpsPrice);
-    $('.vpsCore span', '#custom-plan').text(vpsObj.vpsCore);
-    $('.vpsMemory span', '#custom-plan').text(vpsObj.vpsMemory);
-    $('.vpsStorage span', '#custom-plan').text(vpsObj.vpsStorage);
-    $('.vpsBandwidth span', '#custom-plan').text(vpsObj.vpsBandwidth);
-    $('.vpsWHmcsUrl', '#custom-plan').attr("href", vpsObj.vpsWHmcsUrl);
-  } // 6. monthly, yearly, biannual and triennial pricing switch
-
-
-  if ($(".billingCycle").length > 0) {
-    var billingPlanInputs = $("input[name='billingPlan']");
-    billingPlanInputs.change(function () {
-      var billingPlan = $(this).val();
-      $.each(['.monthly-price', '.yearly-price', '.biannual-price', '.triennial-price'], function (index, tag) {
-        $(tag).css('display', 'none');
-      });
-      $('.' + billingPlan + '-price').css('display', 'block');
-    });
-  } // 7. tooltip
-
-
+  // 7. tooltip
   $('.custom-map-location li span').tooltip('show'); // 8. magnify popup video
 
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
@@ -532,64 +365,5 @@ jQuery(function ($) {
       }
     });
   }
-
-  function submitForm() {
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-    var csrfToken = $("#csrfToken").val();
-
-    if (csrfToken) {
-      if (name && email && message) {
-        $.ajax({
-          type: "POST",
-          url: "libs/contact-form-process.php",
-          data: "name=" + name + "&email=" + email + "&message=" + message + "&csrfToken=" + csrfToken,
-          success: function success(text) {
-            if (text == "success") {
-              formSuccess();
-            } else {
-              submitMSG(false, text);
-            }
-          }
-        });
-      } else {
-        submitMSG(false, "Please enter the right information.");
-      }
-    } else {
-      submitMSG(false, "Invalid Token");
-    }
-  }
-
-  function formSuccess() {
-    $("#contactForm")[0].reset();
-    submitMSG(true);
-  }
-
-  function submitMSG(valid, msg) {
-    if (valid) {
-      $(".message-box").removeClass('d-none').addClass('d-block ');
-      $(".message-box div").removeClass('alert-danger').addClass('alert-success').text('Form submitted successfully');
-    } else {
-      $(".message-box").removeClass('d-none').addClass('d-block ');
-      $(".message-box div").removeClass('alert-success').addClass('alert-danger').text('Found error in the form. Please check again.');
-    }
-  }
-
-  function setCsrf() {
-    $.ajax({
-      url: 'libs/csrf.php',
-      type: "GET",
-      dataType: "json",
-      success: function success(data) {
-        if (data) {
-          document.getElementById("csrfToken").value = data.csrfToken;
-        }
-      },
-      error: function error(_error) {
-        console.log("Error " + _error);
-      }
-    });
-  }
-}); // JQuery end
+});
+  // JQuery end
