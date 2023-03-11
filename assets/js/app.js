6890b18ee189acb3bb4f4ff3db1fe0a31feaa036
MustaqeemIAli <109740034+MustaqeemIAli@users.noreply.github.com>
2023-03-12 01:14:39 +0500

    changes made

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,15 +21,95 @@
 // 18. chat api js
 // 19. image gallery js
 // 20. contact form js
-// Open Side bars
-$(document).ready(function(){
-  $('#openNav').click(function() {
-    $('.side_tabs').toggle("slide");
+// New Tab Content
+const $tabsToDropdown = $(".tabs-to-dropdown");
+
+function generateDropdownMarkup(container) {
+  const $navWrapper = container.find(".nav-wrapper");
+  const $navPills = container.find(".nav-pills");
+  const firstTextLink = $navPills.find("li:first-child a").text();
+  const $items = $navPills.find("li");
+  const markup = `
+    <div class="dropdown d-md-none">
+      <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        ${firstTextLink}
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> 
+        ${generateDropdownLinksMarkup($items)}
+      </div>
+    </div>
+  `;
+  $navWrapper.prepend(markup);
+}
+
+function generateDropdownLinksMarkup(items) {
+  let markup = "";
+  items.each(function () {
+    const textLink = $(this).find("a").text();
+    markup += `<a class="dropdown-item" href="#">${textLink}</a>`;
   });
+
+  return markup;
+}
+
+function showDropdownHandler(e) {
+  // works also
+  //const $this = $(this);
+  const $this = $(e.target);
+  const $dropdownToggle = $this.find(".dropdown-toggle");
+  const dropdownToggleText = $dropdownToggle.text().trim();
+  const $dropdownMenuLinks = $this.find(".dropdown-menu a");
+  const dNoneClass = "d-none";
+  $dropdownMenuLinks.each(function () {
+    const $this = $(this);
+    if ($this.text() == dropdownToggleText) {
+      $this.addClass(dNoneClass);
+    } else {
+      $this.removeClass(dNoneClass);
+    }
+  });
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const $this = $(this);
+  const index = $this.index();
+  const text = $this.text();
+  $this.closest(".dropdown").find(".dropdown-toggle").text(`${text}`);
+  $this
+    .closest($tabsToDropdown)
+    .find(`.nav-pills li:eq(${index}) a`)
+    .tab("show");
+}
+
+function shownTabsHandler(e) {
+  // works also
+  //const $this = $(this);
+  const $this = $(e.target);
+  const index = $this.parent().index();
+  const $parent = $this.closest($tabsToDropdown);
+  const $targetDropdownLink = $parent.find(".dropdown-menu a").eq(index);
+  const targetDropdownLinkText = $targetDropdownLink.text();
+  $parent.find(".dropdown-toggle").text(targetDropdownLinkText);
+}
+
+$tabsToDropdown.each(function () {
+  const $this = $(this);
+  const $pills = $this.find('a[data-toggle="pill"]');
+
+  generateDropdownMarkup($this);
+
+  const $dropdown = $this.find(".dropdown");
+  const $dropdownLinks = $this.find(".dropdown-menu a");
+
+  $dropdown.on("show.bs.dropdown", showDropdownHandler);
+  $dropdownLinks.on("click", clickHandler);
+  $pills.on("shown.bs.tab", shownTabsHandler);
 });
+
 // Hamburger
-$('.hamburger').click(function() {
-  $( this ).toggleClass( "active" );
+$('.hamburger').click(function () {
+  $(this).toggleClass("active");
 });
 // dark mode/ light mode
 $(document).ready(function () {
@@ -115,7 +195,7 @@ jQuery(function ($) {
     pageContainer: $('.container'),
     breakpoint: 767.98,
     hideTimeOut: 0
-  }); 
+  });
 
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > $(window).height()) {
@@ -138,7 +218,7 @@ jQuery(function ($) {
         }
       });
     }
-  }); 
+  });
 
   // 7. tooltip
   $('.custom-map-location li span').tooltip('show'); // 8. magnify popup video
